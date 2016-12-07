da3260d11171b3482ccd376209a6fdfadec7316e
Kevin T Bai <kbai77t@berkeley.edu>
2016-12-06 23:42:13 -0800

    Finished part 1

--- perceptron.py
+++ perceptron.py
@@ -1,17 +1,20 @@
-import numpy as np 
+import numpy as np
 import data_classification_utils
 from util import raiseNotDefined
 import random
+import util
 
 class Perceptron(object):
     def __init__(self, categories, numFeatures):
-        """categories: list of strings 
+        """categories: list of strings
            numFeatures: int"""
         self.categories = categories
         self.numFeatures = numFeatures
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
+        self.weights = {}
+        for category in self.categories:
+            self.weights[category] = np.zeros(self.numFeatures)
 
 
     def classify(self, sample):
@@ -19,16 +22,21 @@ class Perceptron(object):
            returns: category with maximum score, must be from self.categories"""
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
+        vector = util.Counter()
+        for category in self.categories:
+            vector[category] = self.weights[category].dot(sample)
+        return vector.argMax()
 
 
     def train(self, samples, labels):
         """samples: np.array of shape (numSamples, numFeatures)
-           labels: list of numSamples strings, all of which must exist in self.categories 
+           labels: list of numSamples strings, all of which must exist in self.categories
            performs the weight updating process for perceptrons by iterating over each sample once."""
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
-
-
-
+        for i, sample in enumerate(samples):
+            true_label = labels[i]
+            predicated_label = self.classify(sample)
+            if true_label != predicated_label:
+                self.weights[true_label] += sample
+                self.weights[predicated_label] -= sample
